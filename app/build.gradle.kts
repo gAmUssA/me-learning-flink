209d1e52888a2336b70b5d48ec2de6f456832d8a
Viktor Gamov <gamussa@gmail.com>
2024-10-17 12:41:33 -0400

    updated dependencies according to official flink docs and Gradle docs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,16 @@
 plugins {
     application
+    java
+    id("com.gradleup.shadow") version "8.3.3"
 }
 
 repositories {
     mavenCentral()
 }
 
-group = "com.example"
-version = "1.0-SNAPSHOT"
+group = "io.confluent.developer"
+version = "0.1-SNAPSHOT"
+description = "Flink Job"
 
 repositories {
     mavenCentral()
@@ -15,23 +18,36 @@ repositories {
 
 val flinkVersion = "1.20.0"
 val testcontainersVersion = "1.20.2"
+val slf4jVersion = "1.7.36"
+val log4jVersion = "2.17.1"
+
+configurations {
+    create("flinkShadowJar")
+}
 
 dependencies {
     // Flink dependencies
     implementation("org.apache.flink:flink-java:$flinkVersion")
     implementation("org.apache.flink:flink-streaming-java:$flinkVersion")
     implementation("org.apache.flink:flink-clients:$flinkVersion")
+    // end Flink dependencies
+
     implementation("net.datafaker:datafaker:2.4.0")
 
-    implementation("org.slf4j:slf4j-api:2.0.7")
-    implementation("ch.qos.logback:logback-classic:1.4.11")
+    runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
+    runtimeOnly("org.apache.logging.log4j:log4j-api:$log4jVersion")
+    runtimeOnly("org.apache.logging.log4j:log4j-core:$log4jVersion")
 
     // Test dependencies
+    // Flink Testing
     testImplementation("org.apache.flink:flink-test-utils:$flinkVersion")
     testImplementation("org.apache.flink:flink-connector-test-utils:$flinkVersion")
+    // TC
+    // TODO: add Kafka
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    
     testImplementation("org.testcontainers:testcontainers:$testcontainersVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
     testImplementation("org.testcontainers:junit-jupiter:$testcontainersVersion")
 
 
@@ -51,4 +67,30 @@ tasks.named<Test>("test") {
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
+}
+
+sourceSets["main"].run {
+    compileClasspath += configurations["flinkShadowJar"]
+    runtimeClasspath += configurations["flinkShadowJar"]
+}
+sourceSets["test"].run {
+    compileClasspath += configurations["flinkShadowJar"]
+    runtimeClasspath += configurations["flinkShadowJar"]
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.named<Jar>("jar") {
+    manifest {
+        attributes(
+            "Built-By" to System.getProperty("user.name"),
+            "Build-Jdk" to System.getProperty("java.version")
+        )
+    }
+}
+
+tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+    configurations = listOf(project.configurations["flinkShadowJar"])
 }
